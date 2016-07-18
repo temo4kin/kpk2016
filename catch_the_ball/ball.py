e48a1e49da7d585eb19e47f05421332bfe47d483
Артем Калашников <temo4kin@bk.ru>
2016-07-18 15:03:40 +0300

    Добавление правил для пользователя Добавлены инструкции к игре для пользователя. P.S. Идет работа над выбором случайного цвета для шарика, но с учетом того, что определенный цвет приновит определенное количество баллов.

--- catch_the_ball/ball.py
+++ catch_the_ball/ball.py
@@ -2,9 +2,11 @@ import tkinter
 from random import choice, randint
 
 ball_initial_number = 20
-ball_minimal_radius = 20
+ball_minimal_radius = 15
 ball_maximal_radius = 40
-ball_available_colors = ['green', 'blue', 'red', 'lightgray', '#FF00FF', '#FFFF00']
+ball_available_colors = '0123456789ABCDEF'#['green', 'blue', 'red', 'lightgray', '#FF00FF', '#FFFF00']
+balls_coord = []#список координат шариков
+balls_num = []#список номеров шариков
 
 def click_ball(event):
     """ Обработчик событий мышки для игрового холста canvas
@@ -12,39 +14,63 @@ def click_ball(event):
     По клику мышкой нужно удалять тот объект, на который мышка указывает.
     А также засчитываеть его в очки пользователя.
     """
+    global points, label,  balls_coord, balls_num
     obj = canvas.find_closest(event.x, event.y)
     x1, y1, x2, y2 = canvas.coords(obj)
-
+    num = obj[0]# вытаскиваем номер объекта из кортежа
     if x1 <= event.x <= x2 and y1 <= event.y <= y2:
         canvas.delete(obj)
-        # FIXME: нужно учесть объект в очках
+        index = balls_num.index(num)# определяем индекс элемента списка, где храниться номер объекта
+        balls_num.pop(index)# удаляем элемент списка с номером объекта
+        balls_coord.pop(index)# удаляем элемент списка с координатами объекта
+        points+=1
+        label['text']=points
         create_random_ball()
 
 
 def move_all_balls(event):
     """ передвигает все шарики на чуть-чуть
-    """
     for obj in canvas.find_all():
         dx = randint(-1, 1)
         dy = randint(-1, 1)
-        canvas.move(obj, dx, dy)
+        canvas.move(obj, dx, dy)"""
+    global balls_coord
+    """каждый шарик движется по своей траектории"""
+    for obj in balls_coord:
+        x1, y1, x2, y2 =canvas.coords(obj[0])
+        # проверяем, не выйдет ли шарик за границы холста
+        if x1+obj[1]+obj[3]>=400 or x1+obj[1]<=0:
+            obj[1]=-obj[1] #меняем направление движения
+        if y1+obj[2]+obj[3]>=400 or y1+obj[2]<=0:
+            obj[2]=-obj[2]
+        canvas.move(obj[0],obj[1],obj[2])
 
 def create_random_ball():
     """
     создаёт шарик в случайном месте игрового холста canvas,
      при этом шарик не выходит за границы холста!
     """
+    global balls_coord, balls_num
     R = randint(ball_minimal_radius, ball_maximal_radius)
     x = randint(0, int(canvas['width'])-1-2*R)
     y = randint(0, int(canvas['height'])-1-2*R)
-    canvas.create_oval(x, y, x+2*R, y+2*R, width=0, fill=random_color())
-
+    #рисуем шарик и запоминаем его номер в num_oval
+    num_oval = canvas.create_oval(x, y, x+R, y+R, width=0, fill=random_color())
+    dx = randint(-2, 2)
+    dy = randint(-2, 2)
+    # запоминаем идентификатор, вектор и радиус движения нового шарика
+    balls_coord.append([num_oval, dx, dy, R])
+    balls_num.append(num_oval)# запоминаем номер нового шарика
 
 def random_color():
     """
     :return: Случайный цвет из некоторого набора цветов
     """
-    return choice(ball_available_colors)
+    #return choice(ball_available_colors)
+    color = '#'
+    for c in range(6):
+        color = color + choice(ball_available_colors)
+    return color
 
 
 def init_ball_catch_game():
@@ -55,17 +81,34 @@ def init_ball_catch_game():
         create_random_ball()
 
 def init_main_window():
-    global root, canvas
+    global root, canvas, label, points
 
     root = tkinter.Tk()
+    label_text = tkinter.Label(root, text = 'Набранные очки')
+    label_text.pack()
+    points = 0
+    label = tkinter.Label(root, text=points)#привязка к переменной
+    label.pack()
     canvas = tkinter.Canvas(root, background='white', width=400, height=400)
     canvas.bind("<Button>", click_ball)
     canvas.bind("<Motion>", move_all_balls)
     canvas.pack()
+    label_text = tkinter.Label(root, text = 'За бонусные цвета даётся по 2 очка:')
+    label_text.pack()
+    canvas2 = tkinter.Canvas(root)
+    canvas2 = tkinter.Canvas(root)
+    canvas2.create_rectangle(10,10,30,30,fill="white", width=0)
+    canvas2.create_rectangle(40,10,60,30,fill="red", width=0)
+    canvas2.create_rectangle(70,10,90,30,fill="blue", width=0)
+    canvas2.create_rectangle(100,10,120,30,fill="green", width=0)
+    canvas2.pack()
+    canvas2.pack(side='bottom')
+
+
 
 
 if __name__ == "__main__":
     init_main_window()
     init_ball_catch_game()
     root.mainloop()
-    print("Приходите поиграть ещё!")
+    print("Ваш счет:", points)
